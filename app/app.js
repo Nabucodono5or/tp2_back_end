dd41064a44956fcc793534a5177fe4dde6ff2033
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-05-17 21:58:44 -0300

    ui-router funcionando

--- app/app.js
+++ app/app.js
@@ -2,4 +2,16 @@
 
   angular.module('myApp', ['ui.router']);
 
-})()
+})();
+
+(function () {
+  angular.module('myApp').config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/home');
+    $stateProvider.state( 'home', {
+      url: '/home',
+      controller: 'homeController',
+      controllerAs: '$ctrl',
+      templateUrl: 'home/home.html'
+    });
+  });
+})();
